60095b015391c9467198ed06ede0b3e5628fed6a
Jae-hong-Lee <keash8091@gmail.com>
2023-05-23 21:53:16 +0900

    firebase edit

--- renderer/src/common/firebase/firebase.ts
+++ renderer/src/common/firebase/firebase.ts
@@ -18,5 +18,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
+// storage , realtimeDB
 
 export { auth, db, storage };
